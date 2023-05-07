64e2a162723c583c41f5558a62b14385fdc06dfc
RieYamada <rie0615y@gmail.com>
2023-05-07 12:26:47 +0900

    Add:userのルート

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'top', to: 'top#index'
   get 'inquiry', to: 'inquiry#index'
+  resources :users, only: %i[new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
