d0fd396d88766e07217e5c285a28fb9d562840f6
RieYamada <rie0615y@gmail.com>
2023-05-07 19:55:40 +0900

    feat:logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'top', to: 'top#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'contact', to: 'contact#index'
   resources :users, only: %i[new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
