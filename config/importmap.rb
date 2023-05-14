3c4432377ca5d6aadf52fa742137b0d6ceaa0607
RieYamada <rie0615y@gmail.com>
2023-05-14 10:26:45 +0900

    style:mypage

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
 pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.0-alpha2/lib/assets/compiled/rails-ujs.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
