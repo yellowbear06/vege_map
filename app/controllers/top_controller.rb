a19ee12cdd480824e282a2f1b2cb55c975426acb
RieYamada <rie0615y@gmail.com>
2023-06-04 16:03:41 +0900

    style:navbarの編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,7 @@ class TopController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @q = Place.ransack(params[:q])
-    @places = @q.result(distinct: true).order(created_at: :desc).limit(30)
+    @places = Place.all.order(created_at: :desc).limit(30)
     gon.places = @places
   end
 end
